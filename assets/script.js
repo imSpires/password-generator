812b1575da174128053d54d87ddc9c10499d8bbb
Ryan Dungan <rd3@knights.ucf.edu>
2021-07-09 22:30:57 -0400

    added confirmations for special characters/upper/lower/numbers

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,18 @@ function writePw() {
   var specialChars = "!@#$%^&*()_+{}|:?><[]";
 
   // Prompt user to enter char length
-  var pwLength = prompt('How long would you like your password to be? (8-128 characters)')
-
+  var pwLength = prompt('How long would you like your password to be? (8-128 characters)');
+  
+  // Prompt user to enter
   if (pwLength >= minPwLength && pwLength <= maxPwLength) {
-    console.log(pwLength);
+    // let user customize password
+    var cNumbers= confirm("Would you like to use numbers?");
+    var cSpecialChars = confirm("How about special characters?");
+    var cUpper = confirm("Would you like to use uppercase letters?");
+    var cLower = confirm("Lastly, Would you like to use lowercase letters?");
+
+  } else {
+    alert('Password MUST be between 8 and 128 characters')
   }
 }
 
